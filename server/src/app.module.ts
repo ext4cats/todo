2d6f3e9b7785f341360cb41b05bc0232a849fdcc
ext4cats <ext4cats@farmarrays.com>
2025-06-30 19:14:32 -0300

    set up nestjs config module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import * as path from 'node:path';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ cache: true }),
     ServeStaticModule.forRoot({
       rootPath: path.resolve('public'),
       exclude: ['/api/{*splat}'],
